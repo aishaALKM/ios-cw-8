f75dde9bcaf102ca79fe72ea3b937167709ceab7
aishaALKM <70426536+aishaALKM@users.noreply.github.com>
2022-03-12 12:26:26 +0300

    khngxzcvcbn

--- CW1.playground/Contents.swift
+++ CW1.playground/Contents.swift
@@ -3,3 +3,58 @@ import UIKit
 // التمرين الأول
 // ClassWork 1
 
+// Hint: current year - birthday
+//Hello my name is your name and my age is your age
+
+
+
+
+ // var name: String = "aisha"
+
+// var brithdayYear: Int = 2005
+
+ //var stringNumber: String = "aisha"
+ 
+//var birthdayYear = Double(stringNumber) ?? 2022.0
+
+let name = "Aisha"
+let brithdayYear = 2005
+
+
+var nummm = 2022 - brithdayYear
+
+print("Hello my name is \(name),and my age is \(nummm)")
+
+
+//var nubeeer1: String = "8"
+
+//var nubeeer3: Double = 2.5
+
+var stringNumber = "8"
+
+var nubeeer2: Int = 4
+
+var doubleNumber = Double(stringNumber) ?? 2.5
+
+
+
+
+
+var sum = Double(stringNumber) + Double(nubeeer2) + (doubleNumber)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
